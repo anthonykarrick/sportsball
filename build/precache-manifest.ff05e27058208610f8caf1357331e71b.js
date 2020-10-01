a45e8f06ba1c7fcfd706e5dadc1661ed21a38171
akarrick <38015875+anthonykarrick@users.noreply.github.com>
2020-10-01 13:58:12 -0700

    sportsball

---
+++ build/precache-manifest.ff05e27058208610f8caf1357331e71b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ffac929070ec67a8ffb3499aa84706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91f07700f89a41a62f2c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "05454edc6071004e1dc2",
+    "url": "/static/js/2.50adcb46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.50adcb46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f07700f89a41a62f2c",
+    "url": "/static/js/main.27c2c5b7.chunk.js"
+  },
+  {
+    "revision": "688e6ae14072e2564645",
+    "url": "/static/js/runtime-main.851323a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
